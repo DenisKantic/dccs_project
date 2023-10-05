bde187da4b9c09d1df34c84aec973977a8fd532c
DenisKantic <denis.kantic18@gmail.com>
2023-10-05 23:22:47 +0200

    added post method and tested in postman api

--- server/routes/CertRoutes.js
+++ server/routes/CertRoutes.js
@@ -4,7 +4,6 @@ import { CertModel } from '../model/CertModel.js'
 
 const router = express.Router();
 
-{/*
 router.post('/', async (req,res)=>{
     try{
         if(
@@ -12,11 +11,26 @@ router.post('/', async (req,res)=>{
             !req.body.certificateType ||
             !req.body.validFrom || 
             !req.body.validTo
-        )
+        ) { 
+            return res.status(400).send({
+                message: "please send all required fields"
+            })
+        }
+
+        const newCert = {
+            supplier: req.body.supplier,
+            certificateType: req.body.certificateType,
+            validFrom: req.body.validFrom,
+            validTo: req.body.validTo
+        }
+
+        const cert = await CertModel.create(newCert);
+        return res.send(cert);
+    } 
+    catch(error){
+        console.log(error)
     }
 })
-
-*/}
 
 router.get('/', async (req,res)=>{
     try{
